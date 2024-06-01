f36586ed9ce0c4fa801c711610fab55be2d4c938
Sridhar <sridharmoturu@gmail.com>
2024-06-01 12:53:07 +0530

    cors

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(
   cors({
     // origin: "http://localhost:5174",
-    origin: "https://tabular-insight.vercel.app/",
+    origin: "https://tabular-insight.vercel.app",
   })
 );
  app.use(express.json());
